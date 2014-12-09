7c7eac874da172b6dd8f05ee9ca07b751c5ed812
Tom1 <dongtangjin1@gmail.com>
2014-12-09 16:30:34 +0800

    add asset activerecord

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,2 +1,11 @@
+require 'paperclip_processors/watermark'
 class Asset < ActiveRecord::Base
-end
+  has_attached_file :data, :processors => [:watermark],
+  :styles => {
+    :original => {
+      :geometry => '550x400>',
+      :watermark_path => "#{Rails.root}/public/images/rails.png",#水印图片所在位置
+      :position => 'Center' ＃添加的水印在图片哪个位置
+    }
+  }
+end  
