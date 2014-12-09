df072b9985f0618b9eee3f4e7347aceea4f3cb35
Tom1 <dongtangjin1@gmail.com>
2014-12-09 16:41:55 +0800

    fix bug

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,8 +4,8 @@ class Asset < ActiveRecord::Base
   :styles => {
     :original => {
       :geometry => '550x400>',
-      :watermark_path => "#{Rails.root}/public/images/rails.png",#水印图片所在位置
-      :position => 'Center' ＃添加的水印在图片哪个位置
+      :watermark_path => "#{Rails.root}/public/images/rails.png",
+      :position => 'Center'
     }
   }
-end  
+end
