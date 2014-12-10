ec45bb01ba8d957b1866ef1db44dcd3a16071119
Tom1 <dongtangjin1@gmail.com>
2014-12-10 15:54:30 +0800

    fix last error: add code   validates_attachment_content_type :data, :content_type => %w(image/jpeg image/jpg image/png)

--- app/models/weblog_asset.rb
+++ app/models/weblog_asset.rb
@@ -1,4 +1,5 @@
 class WeblogAsset < ActiveRecord::Base
   belongs_to :post
   has_attached_file :data, :url => "/system/:attachment/:id/:style/:filename"
+  validates_attachment_content_type :data, :content_type => %w(image/jpeg image/jpg image/png)
 end
