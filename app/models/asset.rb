21bd011f19b1ae4fcc788bc0120ed4462fe16446
Tom1 <dongtangjin1@gmail.com>
2014-12-11 14:04:13 +0800

    gem 'debugger' bundle update

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,10 +4,10 @@ class Asset < ActiveRecord::Base
   :styles => {
     :original => {
       :geometry => '550x400>',
-      :watermark_path => "#{Rails.root}/public/images/rails.png",
+      :watermark_path => "#{Rails.root}/public/rails.png",
       :position => 'Center'
     }
   }
   validates_attachment_content_type :data, :content_type => %w(image/jpeg image/jpg image/png)
-  
+
 end
